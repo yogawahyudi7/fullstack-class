50f283fc1e82de63d9cee808ded952d7b095aa3d
Yoga Wahyudi <yogawahyudi7@gmail.com>
2022-08-16 16:46:23 +0700

    Feat : Add Feature B #2

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hallo B");
+        
+        
+        
+        System.out.println("hallo B");
+        
     }
 }
