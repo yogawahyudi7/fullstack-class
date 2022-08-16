7900b87112cec5bbb43c140a750931370e73d665
Yoga Wahyudi <yogawahyudi7@gmail.com>
2022-08-16 16:14:43 +0700

    Feat : Add Feature A #3

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hallo A");
-        
-        
+        System.out.println("hallo A2");
+        System.out.println("hallo A3");
         System.out.println("hallo A4");
 
     }
